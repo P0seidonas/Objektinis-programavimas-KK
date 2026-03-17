7aac9803eaffd4b928da3dce75d6816221a7cb85
P0seidonas <henrikasot@gmail.com>
2026-03-18 01:22:35 +0200

    Atliktas trecias laboras

---
+++ lab3/lab3/WeightedAverageStrategy.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace lab3
+{
+    public class WeightedAverageStrategy : IAverageStrategy
+    {
+        public float Calculate(Student student)
+        {
+            return student.Vidurkis * 1.2f;
+        }
+    }
+}
